9cad1deba334c76f59e38d07c88b84d9a5c2998e
chris pryer <christophpryer@gmail.com>
2020-07-23 23:20:30 -0400

    updated for priv. vm

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+// Logistics Engineering library
+// Features Planned:
+//   - dbscan clustering module for geographic coordinate clustering related
+//     to supply chain engineering analysis and modeling.
+//   - kmeans clustering module for geographic center-of-gravity modeling.
+//   - routing solver for transporation optimization.
+//   - scheduler genetic algorithm for driver schedule optimization.
+//   - flow module for network optimization.
+// 
+// Related Projects:
+//   - solverstack
+//
+// Authors:
+//   - fingafrog
